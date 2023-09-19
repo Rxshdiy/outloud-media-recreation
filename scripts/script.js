8085d552dcaabf1a8896e021b2f611e4f156f43b
Rushdiy <rushdiyisaacs@Gmail.com>
2023-09-19 11:44:24 +0200

    finishing typing javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 const typingText = document.querySelector('#first-typing-text');
+
+const textLoad = () => {
+  setTimeout(() => {
+    typingText.textContent = "We Are Outloud Media."
+  }, 0)
+  setTimeout(() => {
+    typingText.textContent = "We Grow Your Audience."
+  }, 4000)
+  setTimeout(() => {
+    typingText.textContent = "We Maximize Revenue."
+  }, 8000)
+  setTimeout(() => {
+    typingText.textContent = "We Unlock Your Potential."
+  }, 12000)
+}
+
+textLoad();
+setInterval(textLoad, 16000)
